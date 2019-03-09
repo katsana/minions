c014b234f089619d36dbe4594cc378cfd8118f5c
Mior Muhammad Zaki <crynobone@gmail.com>
2019-03-09 15:35:23 +0800

    Update docblock.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Server/Message.php
+++ src/Server/Message.php
@@ -2,20 +2,49 @@
 
 namespace Minions\Server;
 
+use React\Http\Response;
+
 class Message
 {
+    /**
+     * Message status.
+     *
+     * @return int
+     */
     public function status(): int
     {
         return 200;
     }
 
+    /**
+     * Message headers.
+     *
+     * @return array
+     */
     public function headers(): array
     {
         return ['Content-Type' => 'text/plain'];
     }
 
+    /**
+     * Message contents.
+     *
+     * @return string
+     */
     public function contents(): string
     {
         return 'Hello world';
+    }
+
+    /**
+     * Convert message to response.
+     *
+     * @return \React\Http|Response
+     */
+    public function asResponse(): Response
+    {
+        return new Response(
+            $this->status(), $this->headers(), $this->contents()
+        );
     }
 }
