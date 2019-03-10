db568a311b35683ba4ab0a21d8bd48c3ac794363
Mior Muhammad Zaki <crynobone@gmail.com>
2019-03-10 21:08:45 +0800

    Add basic tests.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Unit/Client/MessageTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Minions\Tests\Unit\Client;
+
+use Carbon\Carbon;
+use Minions\Client\Message;
+use Mockery as m;
+use PHPUnit\Framework\TestCase;
+
+class MessageTest extends TestCase
+{
+    /**
+     * Teardown the test environment.
+     */
+    protected function tearDown(): void
+    {
+        Carbon::setTestNow(null);
+        m::close();
+    }
+
+    /** @test */
+    public function it_can_be_initiated()
+    {
+        $message = new Message('math/add', [1, 2], 1);
+
+        $this->assertSame('2.0', $message->version());
+        $this->assertSame('math/add', $message->method());
+        $this->assertSame([1, 2], $message->parameters());
+        $this->assertSame(1, $message->id());
+    }
+
+    /** @test */
+    public function it_can_be_converted_to_json()
+    {
+        $message = new Message('math/add', [1, 2], 2);
+
+        $this->assertSame(
+            '{"jsonrpc":"2.0","method":"math\/add","params":[1,2],"id":2}', $message->toJson()
+        );
+    }
+
+    /** @test */
+    public function it_can_generate_correct_signature()
+    {
+        Carbon::setTestNow(Carbon::createFromTimestamp(1546300800));
+        $message = new Message('math/add', [1, 2], 3);
+
+        $this->assertSame(
+            't=1546300800,v1=17f40ac0151055bd2c464a58eafdff28bf25d3118d41005f8209754b26f0e20a', $message->signature('secret')
+        );
+
+        Carbon::setTestNow(Carbon::createFromTimestamp(1546300802));
+
+        $this->assertSame(
+            't=1546300802,v1=cb346293fa89b95ae84ba87a2e11d515c14c61425ac045dff68fde598823c490', $message->signature('secret')
+        );
+    }
+}
---
+++ tests/Unit/Client/NotificationTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Minions\Tests\Unit\Client;
+
+use Carbon\Carbon;
+use Minions\Client\Notification;
+use Mockery as m;
+use PHPUnit\Framework\TestCase;
+
+class NotificationTest extends TestCase
+{
+    /**
+     * Teardown the test environment.
+     */
+    protected function tearDown(): void
+    {
+        Carbon::setTestNow(null);
+        m::close();
+    }
+
+    /** @test */
+    public function it_can_be_initiated()
+    {
+        $message = new Notification('math/add', [1, 2]);
+
+        $this->assertSame('2.0', $message->version());
+        $this->assertSame('math/add', $message->method());
+        $this->assertSame([1, 2], $message->parameters());
+    }
+
+    /** @test */
+    public function it_can_be_converted_to_json()
+    {
+        $message = new Notification('math/add', [1, 2]);
+
+        $this->assertSame(
+            '{"jsonrpc":"2.0","method":"math\/add","params":[1,2]}', $message->toJson()
+        );
+    }
+
+    /** @test */
+    public function it_can_generate_correct_signature()
+    {
+        Carbon::setTestNow(Carbon::createFromTimestamp(1546300800));
+        $message = new Notification('math/add', [1, 2]);
+
+        $this->assertSame(
+            't=1546300800,v1=3c1faf9b318b33b609f612e5e36cd5117fae4f4caf38c6141782b392ed3343d2', $message->signature('secret')
+        );
+
+        Carbon::setTestNow(Carbon::createFromTimestamp(1546300802));
+
+        $this->assertSame(
+            't=1546300802,v1=fb0c53f47a4664d5f45ea0ac32d5c8f6af20d82e1546aa6caae0f7e341c4e998', $message->signature('secret')
+        );
+    }
+}
