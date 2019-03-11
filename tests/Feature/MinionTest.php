8377cb55f91707ffd2ec48fe085bf39e6e4ce9d7
Mior Muhammad Zaki <crynobone@gmail.com>
2019-03-11 19:16:59 +0800

    Test facade.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Feature/MinionTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Minions\Tests\Feature;
+
+use Minions\Minion;
+use Minions\Tests\TestCase;
+
+class MinionTest extends TestCase
+{
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        config([
+            'minions' => ['id' => 'platform'],
+        ]);
+    }
+
+    /** @test */
+    public function it_can_resolve_the_facade()
+    {
+        $minion = Minion::getFacadeRoot();
+
+        $this->assertInstanceOf('Minions\Client\Minion', $minion);
+    }
+}
