f0181af1d19ce359846dc8e5596077b778549748
Mior Muhammad Zaki <crynobone@gmail.com>
2019-03-11 22:06:35 +0800

    Project could be null.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Exceptions/ProjectNotFound.php
+++ src/Exceptions/ProjectNotFound.php
@@ -6,7 +6,7 @@ use Datto\JsonRpc\Exceptions\Exception as JsonRpcException;
 
 class ProjectNotFound extends JsonRpcException
 {
-    public function __construct(string $project)
+    public function __construct(?string $project)
     {
         parent::__construct("Unable to find project: {$project}", -32600);
     }
