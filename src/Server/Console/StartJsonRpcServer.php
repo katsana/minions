9bd7d76641c98e60b3e6885f7a561e36be90ee05
Mior Muhammad Zaki <crynobone@gmail.com>
2019-03-11 16:18:28 +0800

    Improves booting sequence.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Server/Console/StartJsonRpcServer.php
+++ src/Server/Console/StartJsonRpcServer.php
@@ -4,6 +4,7 @@ namespace Minions\Server\Console;
 
 use Error;
 use Exception;
+use React\EventLoop\LoopInterface;
 use Illuminate\Console\Command;
 use Psr\Http\Message\ServerRequestInterface;
 use React\EventLoop\Factory as EventLoop;
@@ -32,6 +33,7 @@ class StartJsonRpcServer extends Command
             'secure' => false,
         ]);
 
+        $host = $config['host'] ? '0.0.0.0';
         $port = $config['port'];
 
         $loop = EventLoop::create();
@@ -51,15 +53,42 @@ class StartJsonRpcServer extends Command
         });
 
         if ($config['secure'] === true) {
-            $server->listen(new SocketServer("tls://0.0.0.0:{$config['port']}", $loop, $config['options']));
-
-            echo "Server running at https://0.0.0.0:{$config['port']}\n";
+            $this->bootSecuredServer($server, $loop, "{$host}:{$port}", $config['options'] ?? []);
         } else {
-            $server->listen(new SocketServer("0.0.0.0:{$config['port']}", $loop));
-
-            echo "Server running at http://0.0.0.0:{$config['port']}\n";
+            $this->bootUnsecuredServer($server, $loop, "{$host}:{$port}");
         }
 
         $loop->run();
+    }
+
+    /**
+     * Boot HTTPS Server.
+     *
+     * @param  \React\Http\Server    $server
+     * @param  \React\EventLoop\LoopInterface $loop
+     * @param  string        $hostname
+     * @param  array         $options
+     * @return void
+     */
+    protected function bootSecuredServer(HttpServer $server, LoopInterface $loop, string $hostname, array $options): void
+    {
+        $server->listen(new SocketServer("tls://{$host}:{$port}", $loop, $config['options']));
+
+        echo "Server running at https://{$hostname}\n";
+    }
+
+    /**
+     * Boot HTTPS Server.
+     *
+     * @param  \React\Http\Server    $server
+     * @param  \React\EventLoop\LoopInterface $loop
+     * @param  string        $hostname
+     * @return void
+     */
+    protected function bootUnsecuredServer(HttpServer $server, LoopInterface $loop, string $hostname): void
+    {
+        $server->listen(new SocketServer("{$hostname}", $loop));
+
+        echo "Server running at http://{$hostname}\n";
     }
 }
