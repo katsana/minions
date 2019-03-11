a1f9082143141094117490ae943a4b7cc7e1fd51
Mior Muhammad Zaki <crynobone@gmail.com>
2019-03-11 23:13:29 +0800

    Add tests.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Unit/Server/Console/Middleware/StatusPageTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Minions\Tests\Unit\Console\Middleware;
+
+use Minions\Server\Console\Middleware\StatusPage;
+use Mockery as m;
+use PHPUnit\Framework\TestCase;
+
+class StatusPageTest extends TestCase
+{
+    /**
+     * Teardown the test environment.
+     */
+    protected function tearDown(): void
+    {
+        m::close();
+    }
+
+    /** @test */
+    public function it_can_show_status_page_on_get_request()
+    {
+        $request = m::mock('Psr\Http\Message\ServerRequestInterface');
+
+        $request->shouldReceive('getMethod')->once()->andReturn('GET')
+            ->shouldReceive('getUri->getPath')->once()->andReturn('/');
+
+        $middleware = new StatusPage();
+
+        $response = $middleware($request, function ($request) {
+            //
+        });
+
+        $this->assertInstanceOf('React\Http\Response', $response);
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertSame(['Content-Type' => ['text/plain']], $response->getHeaders());
+        $this->assertSame('OK', (string) $response->getBody());
+    }
+}
