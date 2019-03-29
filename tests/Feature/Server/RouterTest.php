5ebdcfd25c9ed72efe5420196d69024b232837ed
Mior Muhammad Zaki <crynobone@gmail.com>
2019-03-29 23:38:37 +0800

    Add additional tests.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Feature/Server/RouterTest.php
+++ tests/Feature/Server/RouterTest.php
@@ -79,6 +79,41 @@ class RouterTest extends ServerTestCase
     }
 
     /** @test */
+    public function it_can_dispatch_the_request_when_project_is_not_declared()
+    {
+        Carbon::setTestNow(Carbon::createFromTimestamp(1546300800));
+
+        config(['minions' => [
+            'id' => 'foobar',
+            'projects' => [
+                'demo' => [
+                    'token' => 'secret!',
+                    'signature' => 'secret',
+                ],
+            ],
+            'services' => [
+                'math/add' => ['handler' => 'Minions\Tests\Stubs\AddMath'],
+            ],
+        ]]);
+
+        $request = m::mock('Psr\Http\Message\ServerRequestInterface');
+
+        $request->shouldReceive('getHeader')->once()->with('X-Request-ID')->andReturn(['demo'])
+            ->shouldReceive('hasHeader')->once()->with('Authorization')->andReturn(true)
+            ->shouldReceive('getHeader')->once()->with('Authorization')->andreturn(['Token secret!'])
+            ->shouldReceive('hasHeader')->once()->with('HTTP_X_SIGNATURE')->andReturn(true)
+            ->shouldReceive('getHeader')->once()->with('HTTP_X_SIGNATURE')->andReturn([
+                't=1546300800,v1=17f40ac0151055bd2c464a58eafdff28bf25d3118d41005f8209754b26f0e20a',
+            ])
+            ->shouldReceive('getBody')->once()->andReturn('{"jsonrpc":"2.0","method":"math/add","params":[1,2],"id":3}');
+
+        $reply = $this->app['minions.router']->handle($request);
+
+        $this->assertInstanceOf('Minions\Server\Reply', $reply);
+        $this->assertSame('{"jsonrpc":"2.0","id":3,"result":3}', $reply->body());
+    }
+
+    /** @test */
     public function it_can_dispatch_the_request_when_current_project_is_authorized()
     {
         Carbon::setTestNow(Carbon::createFromTimestamp(1546300800));
