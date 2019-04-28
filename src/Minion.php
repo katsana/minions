f9444de3c00ad81aa42e06f17cd592e341bb3269
Mior Muhammad Zaki <crynobone@gmail.com>
2019-04-28 13:09:28 +0800

    Add docblock typehint.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Minion.php
+++ src/Minion.php
@@ -5,6 +5,11 @@ namespace Minions;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method \Minions\Client\Project project(string $project)
+ * @method \React\Promise\PromiseInterface broadcast(string $project, \Minionts\Client\MessageInterface $message)
+ * @method \React\EventLoop\LoopInterface getEventLoop()
+ * @method \Minions\Client\Minion setEventLoop(\React\EventLoop\LoopInterface $eventLoop)
+ *
  * @see \Minions\Client\Minion
  */
 class Minion extends Facade
