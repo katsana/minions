c34e1f70e69ebda7c4a9d315e729f75ac857ff70
Mior Muhammad Zaki <crynobone@gmail.com>
2019-12-06 21:55:47 +0800

    Add helper methods.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Exceptions/Exception.php
+++ src/Exceptions/Exception.php
@@ -6,5 +6,68 @@ use Datto\JsonRpc\Exceptions\Exception as JsonRpcException;
 
 class Exception extends JsonRpcException
 {
-    //
+    /**
+     * Trigger "Internal error" exception.
+     *
+     * @param  string  $message
+     * @param  mixed  $data
+     *
+     * @return static
+     */
+    public static function internalError(string $message, $data = null)
+    {
+        return new static($message, -32603, $data);
+    }
+
+    /**
+     * Trigger "Parse error" exception.
+     *
+     * @param  string  $message
+     * @param  mixed  $data
+     *
+     * @return static
+     */
+    public static function parseError(string $message, $data = null)
+    {
+        return new static($message, -32700, $data);
+    }
+
+    /**
+     * Trigger "Invalid request" exception.
+     *
+     * @param  string  $message
+     * @param  mixed  $data
+     *
+     * @return static
+     */
+    public static function invalidRequest(string $message, $data = null)
+    {
+        return new static($message, -32600, $data);
+    }
+
+    /**
+     * Trigger "Invalid parameters" exception.
+     *
+     * @param  string  $message
+     * @param  mixed  $data
+     *
+     * @return static
+     */
+    public static function invalidParameters(string $message, $data = null)
+    {
+        return new static($message, -32602, $data);
+    }
+
+    /**
+     * Trigger "Method not found" exception.
+     *
+     * @param  string  $message
+     * @param  mixed  $data
+     *
+     * @return static
+     */
+    public static function methodNotFound(string $message, $data = null)
+    {
+        return new static($message, -32601, $data);
+    }
 }
