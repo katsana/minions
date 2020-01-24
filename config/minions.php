8dd83c46eb6d3642c15ab5a7bbbe22a46b79a6c4
Mior Muhammad Zaki <crynobone@gmail.com>
2020-01-24 21:04:06 +0800

    Remove server related config.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- config/minions.php
+++ config/minions.php
@@ -15,28 +15,6 @@ return [
 
     /*
      |--------------------------------------------------------------------------
-     | Server Configuration
-     |--------------------------------------------------------------------------
-     |
-     | Define the server configuration including port number, SSL support etc.
-     |
-     */
-
-    'server' => [
-        'host' => env('MINION_SERVER_HOST', '127.0.0.1'),
-        'port' => env('MINION_SERVER_PORT', 8085),
-        'secure' => env('MINION_SERVER_SECURE', false),
-        'options' => [
-            'tls' => array_filter([
-                'local_cert' => env('MINION_SERVER_TLS_CERT', null),
-                // 'crypto_method' => STREAM_CRYPTO_METHOD_TLSv1_2_SERVER
-            ]),
-        ],
-    ],
-
-
-    /*
-     |--------------------------------------------------------------------------
      | Projects
      |--------------------------------------------------------------------------
      |
