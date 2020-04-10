9b868f69436b72064461a63ca2e4f5f5eba19a88
Mior Muhammad Zaki <crynobone@gmail.com>
2020-04-10 23:38:24 +0800

    Log exception with custom message and context.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Exceptions/RequestException.php
+++ src/Exceptions/RequestException.php
@@ -2,6 +2,7 @@
 
 namespace Minions\Exceptions;
 
+use Illuminate\Support\Facades\Log;
 use Minions\Client\ResponseInterface;
 use RuntimeException;
 
@@ -59,5 +60,16 @@ class RequestException extends RuntimeException
     public function getRpcErrorData()
     {
         return $this->response->getRpcErrorData();
+    }
+
+    /**
+     * Report error to logger.
+     */
+    public function report(): void
+    {
+        Log::error($this->getMessage(), [
+            'rpc-error' => $this->getRpcErrorData(),
+            'exception' => $this,
+        ]);
     }
 }
